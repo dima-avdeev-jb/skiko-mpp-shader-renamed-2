6647e9cc5c90d2c5224ae97c9a52b9e424d92e29
dima.avdeev <dima.avdeev@jetbrains.com>
2022-05-16 12:12:35 +0400

    flag shader

--- src/commonMain/kotlin/org/jetbrains/skiko/sample/Clocks.kt
+++ src/commonMain/kotlin/org/jetbrains/skiko/sample/Clocks.kt
@@ -7,23 +7,61 @@ import org.jetbrains.skia.paragraph.ParagraphStyle
 import org.jetbrains.skia.paragraph.TextStyle
 import org.jetbrains.skiko.*
 
-val shader1: Shader = RuntimeEffect.makeForShader(
-    """
-    half4 main(float2 fragCoord) {
-      //float2 scaled = fragCoord/iResolution.xy;
-      return half4(1, 0.5, 0, 1);
-    }
+//Thanks to https://www.shadertoy.com/view/7sBBRV
+val sksl = """
+uniform float2 iResolution;
+uniform float iTime;
+uniform shader content;
+float hash13(vec3 p3) {
+    p3  = fract(p3 * .1031);
+    p3 += dot(p3, p3.zyx + 31.32);
+    return fract((p3.x + p3.y) * p3.z);
+}
+half4 main(float2 fragCoord) {
+vec2 uv = fragCoord/iResolution.xy;
+uv = (uv-0.5) *(1.2 - uv.x*0.05) + vec2(0.5, 0.5);
+vec3 windSeed =  vec3(uv.x * 5.,uv.y * 7., iTime);
+float BORDER = 0.1;
+float pole = smoothstep(BORDER, 1., uv.x);
+pole = 1. - pow(1.-pole,6.);
+float windForce = 0.2 + abs(cos(iTime * 0.2 + pole)  * cos(iTime * 0.345));
+float flagUp = 1.-windForce;
+uv.y += pole * (flagUp * 0.1 - 0.06);
+uv.x -= pole * flagUp * 0.025; // fabric stretching -> flag length
+vec3 gyrPos = vec3(uv.x * (1. + pole * pole *2.), uv.y, iTime * 0.4) * 3.;
+float gyr = smoothstep(0.,1., abs(dot(sin(gyrPos), cos(gyrPos.zxy))));
+float wind = 
+    sin(iTime * (10.45) + gyr - uv.x*(20.))*windForce* 0.5
+    // Wind phases 
+   +cos(iTime * 1.23 - uv.y * 3. + uv.x * 3.) * 0.5; // perspective waving
+uv += wind*0.03 * pole;
+float w = 1.0; //length(vec2(dFdx(uv.x), dFdy(uv.y)));
+float isFlag = smoothstep(BORDER,BORDER + .01, 0.5- abs(uv.x-0.5)) * smoothstep(BORDER,BORDER + .01, 0.5- abs(uv.y-0.5));
+float shadowThickness = 0.3 * (1. + wind*0.15);
+vec2 shadowUv = (uv-0.5)*1.3 + vec2(-0.15, 0.15);
+
+float shadow = smoothstep(BORDER - shadowThickness, BORDER, 0.5 - abs(shadowUv.x)) *
+  smoothstep(BORDER - shadowThickness, BORDER, 0.5 - abs(shadowUv.y));
+float isBlue = (1.0 + sign(0.5 - uv.y))/2.0;
+vec3 col = mix(vec3(1., 0.84, 0.) , vec3(0.,0.34,0.71), isBlue); // Yelow Blue
+float upaEdge = cos(iTime);// 0.2;
+col = sqrt(col);
+col.rgb *= (0.8 + wind * 0.3 * pole); // shadow
+col.rgb += 0.1 * pow(max(0., wind),7. + 10.) * vec3(0.3, 0.3, 0.6);  // blick
+float bg = 0.3;
+col.rgb = mix(vec3(bg) * (2. - shadow), col.rgb, isFlag);
+float noise =  hash13(vec3(fragCoord * 15.0, iTime));
+col.rgb += vec3(noise) * 0.03; // hide color range limit on shadows
+col.rgb += col.gbr*col.brg * 0.3; // Bloom
+return half4(col,1.0);
+}
 """
-).makeShader(
-    uniforms = null,//Data.makeFromBytes(),
-    children = null,
-    localMatrix = null,
-    isOpaque = false
-)
+
+val runtimeEffect = RuntimeEffect.makeForShader(sksl)
+val shaderBuilder = RuntimeShaderBuilder(runtimeEffect)
 
 class Clocks(private val layer: SkiaLayer): SkikoView {
     private val cursorManager = CursorManager()
-    private val withFps = true
     private val fpsCounter = FPSCounter()
     private var xpos = 0.0
     private var ypos = 0.0
@@ -37,23 +75,31 @@ class Clocks(private val layer: SkiaLayer): SkikoView {
     private var inputText = ""
 
     override fun onRender(canvas: Canvas, width: Int, height: Int, nanoTime: Long) {
-        if (withFps) fpsCounter.tick()
-        canvas.translate(xOffset.toFloat(), yOffset.toFloat())
-        canvas.scale(scale.toFloat(), scale.toFloat())
+        fpsCounter.tick()
+        val w = minOf(width, height).toFloat()
+        val h = minOf(width, height).toFloat()
+        shaderBuilder.uniform("iTime", (nanoTime % 1_000_000_000_000) / 1e9f)
+        shaderBuilder.uniform("iResolution", w, h)
+        val imageFilter2 = ImageFilter.makeRuntimeShader(
+            runtimeShaderBuilder = shaderBuilder,
+            shaderName = "content",
+            input = null
+        )
 
         val watchFill = Paint().apply {
             color
-            shader = shader1
+            shader
+            imageFilter = imageFilter2
         }
-        canvas.drawRect(Rect(0f, 0f, width.toFloat(), width.toFloat()), watchFill)
+        canvas.drawRect(Rect(0f, 0f, w, h), watchFill)
 
         val frames = ParagraphBuilder(style, fontCollection)
             .pushStyle(TextStyle().setColor(0xffFFffFFL.toInt()).setFontSize(24f))
-            .addText("Peace to Ukraine!")
+            .addText("Peace to Ukraine!   ${fpsCounter.average}")
             .popStyle()
             .build()
         frames.layout(Float.POSITIVE_INFINITY)
-        frames.paint(canvas, ((xpos - xOffset) / scale).toFloat(), ((ypos - yOffset) / scale).toFloat())
+        frames.paint(canvas, 0f, 0f)
 
         canvas.resetMatrix()
     }
